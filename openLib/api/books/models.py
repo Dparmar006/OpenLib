a3fbcb8e659bce6c52b2a0a84e8365cc963f514c
Dixit Parmar <Dparmar006@users.noreply.github.com>
2020-10-11 13:24:36 +0530

    issue solved with terminal

--- openLib/api/books/models.py
+++ openLib/api/books/models.py
@@ -16,7 +16,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default='Anonymous')
     email = models.EmailField(max_length=256, unique=True)
 
     username = None
